02ab6fa6a70e97bffe2b6f192ae406e5acd5deaf
varun-perpetualny <varun@perpetualny.com>
2019-06-26 15:28:38 +0530

    added gitlab ci yml file

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,7 +2,8 @@
 
 export default {
     URL : {
-        LAMBDA_HOST_URL: "https://uedyhinf4i.execute-api.us-east-1.amazonaws.com/dev"
+        LAMBDA_HOST_URL: "https://6ao7l2j7gf.execute-api.us-east-1.amazonaws.com/dev"
+        //LAMBDA_HOST_URL: "https://yy1zdiktrd.execute-api.us-east-1.amazonaws.com/prod"
         //LAMBDA_HOST_URL:"http://localhost:3000"
     }
 }
